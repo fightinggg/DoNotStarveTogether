d5f06271b9e9866c4c0abaa415dc1771f79b755e
fightinggg <42932982+fightinggg@users.noreply.github.com>
2023-03-09 01:03:13 +0800

    Update modoverrides.lua

--- data/DoNotStarveTogether/Cluster_1/Master/modoverrides.lua
+++ data/DoNotStarveTogether/Cluster_1/Master/modoverrides.lua
@@ -1,4 +1,21 @@
 return {
-["workshop-378160973"] = { enabled = true },
-["workshop-2287303119"] = { enabled = true }
+  ["workshop-2287303119"]={
+    configuration_options={
+      [""]=0,
+      T_crop=true,
+      chestB=-1,
+      chestG=-1,
+      chestR=-1,
+      display_hp=1,
+      food_estimation=-1,
+      food_order=0,
+      food_style=0,
+      item_info_mod=0,
+      lang="Auto",
+      show_buddle_item=1,
+      show_food_units=-1,
+      show_uses=-1 
+    },
+    enabled=true 
+  } 
 }
